82bcd9835638462c222b5a0abc6b42566fa5f0ea
Paulina Podraza <paulina.podraza92@gmail.com>
2021-08-24 20:07:21 +0200

    Add application functionality

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,4 @@
+/*eslint-disable*/
 /* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
 {
   'use strict';
@@ -12,34 +13,96 @@
       images: '.books-list .book__image',
     }
   };
+  const classNames = {
+    books: {
+      favoriteBook: 'favorite',
+      filters: '.filters',
+    }
+  };
   const templates = {
-    bookTemplate: Handlebars.compile(document.querySelector('#template-book').innerHTML),
+    bookTemplate: Handlebars.compile(document.querySelector(select.templateOf.bookTemplate).innerHTML),
   };
   function render() {
-      const thisBook = this;
+    const thisBook = this;
     //pętla po każdym elemencie z dataSource.books
     for (let book of dataSource.books) {
-      console.log(book);
-      const generatedHTML = templates.bookTemplate();
+      const generatedHTML = templates.bookTemplate(book);
       thisBook.element = utils.createDOMFromHTML(generatedHTML);
       const booksListContainer = document.querySelector(select.containerOf.booksList);
       booksListContainer.appendChild(thisBook.element);
     }
   }
-    render();
+  render();
 
-    let favoriteBooks = [];
-    initActions(){
-        const booksImage = document.querySelectorAll(select.booksImages.images);
-        for (let image of booksImage) {
-            image.addEventListener('dblclick', function (event) {
-                event.preventDefault();
-            });
-            image.classList.add('favorite');
-            const idBook = image.getAttribute('data-id');
-           favoriteBooks.push(idBook);
+  let favoriteBooks = [];
+  let filters = [];
+  const filtr = document.querySelector('.filters');
+  
+  function initActions() {
+    const booksImage = document.querySelectorAll(select.booksImages.images);
+    for (let image of booksImage) {
+      const idBook = image.getAttribute('data-id');
+      image.addEventListener('dblclick', function (event) {
+        event.preventDefault();
+        const image = event.target.offsetParent;
+        const bookList = favoriteBooks.includes(idBook);
+        if (!bookList) {
+          image.classList.add('favorite');
+          favoriteBooks.push(idBook);
+        }
+        else{
+          // usunięcie z klikniętego elementu klasy favorite
+          image.classList.remove(classNames.books.favoriteBook);
+          // przefiltrowanie tablicy i dodanie nowej bez tego elementu
+          favoriteBooks = favoriteBooks.filter(id => id !== idBook);
+        }
+      });
+    }
+    filtr.addEventListener('click', function (event) {
+      event.preventDefault();
+      if (event.target.tagName === 'INPUT' && event.target.type === 'checkbox' && event.target.name === 'filter') {
+        if (event.target.checked) {
+          filters.push(event.target.value);
+        } else {
+          filters.splice(filters.indexOf(event.target.value));
+        }
+      }
+    });
+  }
+  filterBooks();
+
+  
+  function filterBooks() {
+    for (book of dataSource.books) {
+      let shouldBeHidden = false;
+      for (filter of filters) {
+           if(!book.details[filter]) {
+            shouldBeHidden = true;
+            break;
+        }
+      }
+      if (shouldBeHidden) {
+          const bookCover = document.querySelector('.book__image[data-id="' + book.id + '"]');
+          bookCover.classList.add('hidden');
+        } else {
+          const bookCover = document.querySelector('.book__image[data-id="' + book.id + '"]');
+          bookCover.classList.remove('hidden');
         }
     }
-    initActions();
-
+  }
+  initActions();
+  
+  function determineRatingBgc(rating){
+    let background = '';
+    if (rating < 6) {
+      background = 'linear-gradient(to bottom,  #fefcea 0%, #f1da36 100%)';
+    } else if (rating > 6 && rating <= 8) {
+      background = 'linear-gradient(to bottom, #b4df5b 0%,#b4df5b 100%)';
+    } else if (rating > 8 && rating <= 9) {
+      background = 'linear-gradient(to bottom, #299a0b 0%, #299a0b 100%)';
+    } else if (rating > 9) {
+      background = 'linear-gradient(to bottom, #ff0084 0%,#ff0084 100%)';
+    }
+    return background;
+  }
 }
